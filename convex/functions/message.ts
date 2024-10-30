2b841dc4056494cab1ad8b04d49539da5f65cead
WilMay <wmayhood@outlook.com>
2024-10-30 12:32:09 -0400

    Intialized Convex with messages API

---
+++ convex/functions/message.ts
@@ -0,0 +1,20 @@
+import {mutation,query} from "../_generated/server";
+import { v } from "convex/values";
+
+//query the database, rn it fetches all messages
+//ctx is how the function interacts with db data
+export const list = query({
+  handler: async (ctx) => {
+    return await ctx.db.query("messages").collect();
+  },
+});
+//modify data in the database
+export const create = mutation({
+  args: {
+    sender: v.string(),
+    content: v.string(),
+  },
+  handler: async (ctx, { sender, content }) => {
+    await ctx.db.insert("messages", { sender, content });
+  },
+});
